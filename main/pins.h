c2206e15ef7014c7d906c29c306066cee3262b6c
Rafael Corsi <corsiferrao@gmail.com>
2026-04-10 10:59:22 -0300

    Update pins.h

--- main/pins.h
+++ main/pins.h
@@ -1,13 +1,13 @@
 #ifndef PINS_H
 #define PINS_H
 
-const int BTN_PIN_R = 4;
-const int BTN_PIN_G = 5;
-const int BTN_PIN_B = 6;
-const int LED_PIN_R = 7;
-const int LED_PIN_G = 8;
-const int LED_PIN_B = 9;
+#define BTN_PIN_R 4
+#define BTN_PIN_G 5
+#define BTN_PIN_B 6
+#define LED_PIN_R 7
+#define LED_PIN_G 8
+#define LED_PIN_B 9
 
-const int SSD1306_PIN_LITE = 15;
+#define SSD1306_PIN_LITE 15
 
 #endif // PINS_H

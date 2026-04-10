f2852831a1189115d7f146546c0c444c485b1ed0
Rafael Corsi <corsiferrao@gmail.com>
2026-04-10 10:57:00 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -13,32 +13,20 @@
 #include "pins.h"
 #include "ssd1306.h"
 
-
 // === Definições para SSD1306 ===
 ssd1306_t disp;
 
-volatile int flag_BTN_R = 0;
-volatile int flag_BTN_G = 0;
-volatile int flag_BTN_B = 0;
+#define BTN_QUEUE_LEN 8
 
-// == funcoes de inicializacao ===
+static QueueHandle_t xBtnQueue = NULL;
+
+// === Função de callback (IRQ) ===
 void btn_callback(uint gpio, uint32_t events) {
-    if (events & 0x4) {
-        if (gpio == BTN_PIN_R)
-            flag_BTN_R = 1;
-        else if (gpio == BTN_PIN_G)
-            flag_BTN_G = 1;
-        else if (gpio == BTN_PIN_B)
-            flag_BTN_B = 1;
-    }
+    if (events & GPIO_IRQ_EDGE_RISE) {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    if (events == 0x8) {
-        if (gpio == BTN_PIN_R)
-            flag_BTN_R = 0;
-        else if (gpio == BTN_PIN_G)
-            flag_BTN_G = 0;
-        else if (gpio == BTN_PIN_B)
-            flag_BTN_B = 0;
+        // Envia o GPIO do botão que gerou a interrupção
+        xQueueSendFromISR(xBtnQueue, &gpio, &xHigherPriorityTaskWoken);
     }
 }
 
@@ -60,18 +48,19 @@ void oled_display_init(void) {
 }
 
 void btns_init(void) {
-    // Inicialização dos botões e LEDs
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
     gpio_set_irq_enabled_with_callback(BTN_PIN_R,
                                        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
                                        true, &btn_callback);
+
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
                          true);
+
     gpio_init(BTN_PIN_B);
     gpio_set_dir(BTN_PIN_B, GPIO_IN);
     gpio_pull_up(BTN_PIN_B);
@@ -83,52 +72,72 @@ void led_rgb_init(void) {
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 1);
+
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
     gpio_put(LED_PIN_G, 1);
+
     gpio_init(LED_PIN_B);
     gpio_set_dir(LED_PIN_B, GPIO_OUT);
     gpio_put(LED_PIN_B, 1);
+}
+
+static void handle_button(uint gpio) {
+    if (gpio == BTN_PIN_R) {
+        gpio_put(LED_PIN_R, 0);
+
+        ssd1306_clear(&disp);
+        ssd1306_draw_string(&disp, 8, 24, 2, "RED");
+        ssd1306_show(&disp);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_put(LED_PIN_R, 1);
+    }
+    else if (gpio == BTN_PIN_G) {
+        gpio_put(LED_PIN_G, 0);
+
+        ssd1306_clear(&disp);
+        ssd1306_draw_string(&disp, 8, 24, 2, "GREEN");
+        ssd1306_show(&disp);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_put(LED_PIN_G, 1);
+    }
+    else if (gpio == BTN_PIN_B) {
+        gpio_put(LED_PIN_B, 0);
+
+        ssd1306_clear(&disp);
+        ssd1306_draw_string(&disp, 8, 24, 2, "BLUE");
+        ssd1306_show(&disp);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+        gpio_put(LED_PIN_B, 1);
+    }
 }
 
 void task_1(void *p) {
     oled_display_init();
     btns_init();
     led_rgb_init();
-    
+
+    uint gpio_event;
+
     while (1) {
-        if (flag_BTN_R == 1) {
-            gpio_put(LED_PIN_R, 0);
-            ssd1306_draw_string(&disp, 8, 24, 2, "RED");
-            ssd1306_clear(&disp);
-            ssd1306_show(&disp);
-            gpio_put(LED_PIN_R, 1);
+        if (xQueueReceive(xBtnQueue, &gpio_event, pdMS_TO_TICKS(50)) == pdTRUE) {
+            handle_button(gpio_event);
         }
-
-        if (flag_BTN_G == 1) {
-            gpio_put(LED_PIN_G, 0);
-            ssd1306_draw_string(&disp, 8, 24, 2, "GREEN");
-
-            ssd1306_clear(&disp);
-            ssd1306_show(&disp);
-            gpio_put(LED_PIN_G, 1);
-        }
-
-        if (flag_BTN_B == 1) {
-            gpio_put(LED_PIN_B, 0);
-            ssd1306_draw_string(&disp, 8, 24, 2, "BLUE");
-            ssd1306_clear(&disp);
-            ssd1306_show(&disp);
-            gpio_put(LED_PIN_B, 1);
-            flag_BTN_B = 0;
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
 int main() {
     stdio_init_all();
+
+    xBtnQueue = xQueueCreate(BTN_QUEUE_LEN, sizeof(uint));
+    if (xBtnQueue == NULL) {
+        while (1) {
+            // falha ao criar fila
+        }
+    }
 
     xTaskCreate(task_1, "Task 1", 4095, NULL, 1, NULL);
 
